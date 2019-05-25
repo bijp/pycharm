2038f1ae2f23d0c311495f506309f8e9828f13cf
bijp <312424159@qq.com>
2019-05-25 16:02:18 +0800

    作业目录提交

--- actual_combat/top6.py
+++ actual_combat/top6.py
@@ -3,4 +3,16 @@ get_String 获取用户输入
 print_String 将获取的输入信息大写输出
 请写出类的实现，并分别调用这两个方法"""
 
-import requests
+class IOString():
+    def __init__(self):
+        self.inpu=''
+    def get_String(self):
+        self.inpu=input('请输入信息：')
+
+    def print_String(self):
+        t=self.inpu
+        print(t.upper())
+if __name__ == '__main__':
+    ios = IOString()
+    ios.get_String()
+    ios.print_String()
--- actual_combat/top7.py
+++ actual_combat/top7.py
@@ -2,4 +2,17 @@
 返回城市名和温度，对应的字段分别是city和temp
 要求使用requests模块，返回信息不能是乱码"""
 
+import requests
+
+url='http://www.weather.com.cn/data/sk/101190408.html'
+rep=requests.get(url)
+rep.encoding='utf-8'
+u=rep.text
+#字符串转换字典
+s_dic = eval(u)
+city=s_dic['weatherinfo']['city']
+weath=s_dic['weatherinfo']['temp']
+# print('城市:%s' % city)
+# print('温度:%s' % weath)
+print("%s:%s" % ( city,weath))
 
